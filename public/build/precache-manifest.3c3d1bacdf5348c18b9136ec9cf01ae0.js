f34486584efb35a7e076ae24471bcf7dcf2478c7
Christine Smith <christinecsmith1@gmail.com>
2020-05-25 13:48:13 -0700

    deploy build

---
+++ public/build/precache-manifest.3c3d1bacdf5348c18b9136ec9cf01ae0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f240cbeb560c209aaaab74a8fb2c8fb0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cdc594ad16b2899eb0b7",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "bac6ee61adb0302cd9a0",
+    "url": "/static/js/2.b5cbd303.chunk.js"
+  },
+  {
+    "revision": "b74364752c6386920aced16a152e4b46",
+    "url": "/static/js/2.b5cbd303.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdc594ad16b2899eb0b7",
+    "url": "/static/js/main.8dbab171.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  },
+  {
+    "revision": "4f432d9234988a5f33b26e0ba06bc6fe",
+    "url": "/static/media/loading.4f432d92.gif"
+  },
+  {
+    "revision": "a9b45c8bbb6b8f21d4632b93204a04f7",
+    "url": "/static/media/spacexlogo.a9b45c8b.png"
+  }
+]);
